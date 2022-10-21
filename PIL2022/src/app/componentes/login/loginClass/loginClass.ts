49cef2faa57ff1738af0648a9d55dd3e6a40f6c1
teodc888 <teodc4447@gmail.com>
2022-10-21 17:53:37 -0300

    cambiando strings por any

--- PIL2022/src/app/componentes/login/loginClass/loginClass.ts
+++ PIL2022/src/app/componentes/login/loginClass/loginClass.ts
@@ -3,9 +3,9 @@ export default class LoginClass{
     email: string = ""
     password: string = ""
     monto: number = 0
-    moviminetos: Array<string> = [] 
+    moviminetos: Array<any> = [] 
 
-   constructor(Email: string, Password: string, monto: number, movimientos: Array<string>){
+   constructor(Email: string, Password: string, monto: number, movimientos: Array<any>){
        this.email = Email;
        this.password = Password;
        this.monto= monto;
