eac58c66f353ed1c0fbff3ac4836da8ea39099f3
Joaquin <luduenajoa3@gmail.com>
2022-10-21 17:53:26 -0300

    Deposito route

--- PIL2022/src/app/app-routing.module.ts/app-routing.module.ts.module.ts
+++ PIL2022/src/app/app-routing.module.ts/app-routing.module.ts.module.ts
@@ -3,10 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AcercaDeComponent } from '../componentes/acerca-de/acerca-de.component';
 import { LandingComponent } from '../componentes/landing/landing.component';
 import { QuienesSomosComponent } from '../componentes/quienes-somos/quienes-somos.component';
+import { DepositoComponent } from '../componentes/landing/componentes-landing/deposito/deposito.component';
+
 const routes: Routes = [
   {path: 'home', component: LandingComponent},
   {path: 'quienes-somos', component: QuienesSomosComponent},
   {path: 'acerca-de', component: AcercaDeComponent},
+  {path: 'deposito', component: DepositoComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'}
 ]
 
