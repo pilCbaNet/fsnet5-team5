b251a7ea2e74d6ff258fa9d81d5428375d66fbaa
ezequieleandi <eandiezequiel@gmail.com>
2022-10-26 17:48:25 -0300

    componente retiro

---
+++ PIL2022/src/app/componentes/landing/componentes-landing/retiro/models/retiroClass.ts
@@ -0,0 +1,9 @@
+export class RetiroClass {
+    email: string;
+    monto: number;
+  
+    constructor(email: string, monto: number) {
+      this.email = email;
+      this.monto = monto;
+    }
+  }
