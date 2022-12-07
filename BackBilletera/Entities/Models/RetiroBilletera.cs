a00c6c9db8669e81ad632fc641eabce029377af0
Jacobo <vocosjacobo00@gmail.com>
2022-12-07 19:30:57 -0300

    BilleteraController

---
+++ BackBilletera/Entities/Models/RetiroBilletera.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class RetiroBillertera
+    {
+
+        public int IdBilletera { get; set; }
+        public decimal Saldo { get; set; }
+
+        public int IdUsuario { get; set; }
+    }
+}
---
+++ BackBilletera/MiBilleteraWebApi/Controllers/BilleteraController.cs
@@ -0,0 +1,75 @@
+﻿using Entities.Models;
+using MiBilleteraWebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiBilleteraWebApi.Controllers
+{
+    [Route("api/billetera")]
+    [ApiController]
+    public class BilleteraController : ControllerBase
+    {
+
+        private readonly Pil2022Context context;
+
+        public BilleteraController(Pil2022Context context)
+        {
+            this.context = context;
+        }
+
+        [HttpPost]
+        public ActionResult PostRetiro(RetiroBillertera retiroBillertera)
+        {
+            
+                var retiro = context.Billeteras.FirstOrDefault(x => x.IdBilletera == retiroBillertera.IdBilletera);
+                if (retiroBillertera.Saldo == 0)
+                {
+                    return BadRequest("La billetera NO tiene saldo.");
+                }
+                else
+                {
+                    var montoRetirado = retiro.Saldo - retiroBillertera.Saldo;
+
+                    retiro.Saldo = montoRetirado;
+                    retiro.IdUsuario = retiroBillertera.IdUsuario;
+                    retiro.IdMoneda = 1;
+
+                    context.SaveChanges();
+                    return Ok(retiro);
+                }
+
+
+            
+        }
+
+
+        [HttpPost]
+        [Route("Sumar")]
+        public ActionResult PostIngresar(RetiroBillertera retiroBillertera)
+        {
+            
+            
+                var retiro = context.Billeteras.FirstOrDefault(x => x.IdBilletera == retiroBillertera.IdBilletera);
+                if (retiroBillertera.Saldo == 0)
+                {
+                    return BadRequest("La billetera NO tiene saldo.");
+                }
+                else
+                {
+                    var montoRetirado = retiro.Saldo + retiroBillertera.Saldo;
+
+                    retiro.Saldo = montoRetirado;
+                    retiro.IdUsuario = retiroBillertera.IdUsuario;
+                    retiro.IdMoneda = 1;
+
+                context.SaveChanges();
+                    return Ok(retiro);
+                }
+
+
+            
+        }
+
+    }
+
+}
