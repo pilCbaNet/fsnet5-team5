fcf535b1d9de2f785ad7fe373d9c5fc9aa52ffea
santiago alfredo bruno <tiagoabruno@hotmail.com>
2022-12-07 19:45:18 -0300

    Operaciones Lista Get

---
+++ BackBilletera/MiBilleteraWebApi/Controllers/OperacionesController.cs
@@ -0,0 +1,27 @@
+﻿using MiBilleteraWebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiBilleteraWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperacionesController : ControllerBase
+    {
+        private readonly Pil2022Context context;
+
+        public OperacionesController(Pil2022Context context)
+        {
+            this.context = context;
+        }
+
+            [HttpGet("{id}")]
+            public List<Operacion> Get(int id)
+            {
+
+                    var lista = context.Operaciones.ToList().Where(x => x.IdUsuario == id);
+                    return lista.ToList();
+
+            }
+    }
+}
